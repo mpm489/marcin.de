f4b2dcd284f919b2149ee7d88af53e575b312135
Your Name <you@example.com>
2022-01-17 10:16:42 +0100

    animation for all picteres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  images =['7.jpg', '8.jpg', '10.jpg', '12.jpg' ];
+  images = ['7.jpg', '8.jpg', '10.jpg', '12.jpg'];
   currentImage = 0;
+  showImage = true;
 
-  ngOnInit(){
+  ngOnInit() {
     this.updateImage();
   }
 
 
-  updateImage(){
-    setInterval(()=>{     
-      this.currentImage ++;
-      this.currentImage = this.currentImage % this.images.length;      
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10)
     }, 6000)
   }
 }
