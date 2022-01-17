03db13ada76f4f1b3f6c8fe3f046a7b732170807
Your Name <you@example.com>
2022-01-17 10:04:05 +0100

    Add Images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'marcin';
+export class AppComponent implements OnInit {
+  images =['7.jpg', '8.jpg', '10.jpg', '12.jpg' ];
+  currentImage = 0;
+
+  ngOnInit(){
+    this.updateImage();
+  }
+
+
+  updateImage(){
+    setInterval(()=>{     
+      this.currentImage ++;
+      this.currentImage = this.currentImage % this.images.length;      
+    }, 6000)
+  }
 }
