8f68eed29bf0acc2494524f6159d6433c5309d06
Your Name <you@example.com>
2022-01-20 15:52:52 +0100

    data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
+export class AppComponent{
+
+
+  constructor(public router: Router){}
+
+ 
+
 }
